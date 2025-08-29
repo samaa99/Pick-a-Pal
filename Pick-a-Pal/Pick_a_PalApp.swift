2d0504ca1bf9b32dfcaf877f63fcc21fd05e41de
Sama <samaa@blink.global>
2025-08-29 17:54:01 +0300

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Samaa Soltan on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pick-a-PalTests/Pick_a_PalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalTests.swift
+//  Pick-a-PalTests
+//
+//  Created by Samaa Soltan on 29/08/2025.
+//
+
+import Testing
+@testable import Pick_a_Pal
+
+struct Pick_a_PalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
